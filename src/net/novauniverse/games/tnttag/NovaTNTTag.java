34c88b838171168ab181d9cae1f7549cdeb4d7c6
Anton udén <udenanton@hotmail.com>
2022-05-19 09:31:40 +0200

    Initial commit

---
+++ src/net/novauniverse/games/tnttag/NovaTNTTag.java
@@ -0,0 +1,126 @@
+package net.novauniverse.games.tnttag;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import net.novauniverse.games.tnttag.game.TNTTag;
+import net.zeeraa.novacore.commons.log.Log;
+import net.zeeraa.novacore.commons.utils.JSONFileUtils;
+import net.zeeraa.novacore.spigot.abstraction.events.VersionIndependantPlayerAchievementAwardedEvent;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.GameManager;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.mapselector.selectors.guivoteselector.GUIMapVote;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.gamelobby.GameLobby;
+import net.zeeraa.novacore.spigot.module.ModuleManager;
+
+public class NovaTNTTag extends JavaPlugin implements Listener {
+	private static NovaTNTTag instance;
+
+	public static NovaTNTTag getInstance() {
+		return instance;
+	}
+
+	private boolean allowReconnect;
+	private int reconnectTime;
+
+	private TNTTag game;
+
+	public boolean isAllowReconnect() {
+		return allowReconnect;
+	}
+
+	public int getReconnectTime() {
+		return reconnectTime;
+	}
+
+	public TNTTag getGame() {
+		return game;
+	}
+
+	@Override
+	public void onEnable() {
+		NovaTNTTag.instance = this;
+
+		this.saveConfig();
+
+		allowReconnect = getConfig().getBoolean("allow_reconnect");
+		reconnectTime = getConfig().getInt("player_elimination_delay");
+
+		// Create files and folders
+		File mapFolder = new File(this.getDataFolder().getPath() + File.separator + "Maps");
+		File worldFolder = new File(this.getDataFolder().getPath() + File.separator + "Worlds");
+
+		File mapOverrides = new File(this.getDataFolder().getPath() + File.separator + "map_overrides.json");
+		if (mapOverrides.exists()) {
+			Log.info(getName(), "Trying to read map overrides file");
+			try {
+				JSONObject mapFiles = JSONFileUtils.readJSONObjectFromFile(mapOverrides);
+
+				boolean relative = mapFiles.getBoolean("relative");
+
+				mapFolder = new File((relative ? this.getDataFolder().getPath() + File.separator : "") + mapFiles.getString("maps_folder"));
+				worldFolder = new File((relative ? this.getDataFolder().getPath() + File.separator : "") + mapFiles.getString("worlds_folder"));
+
+				Log.info(getName(), "New paths:");
+				Log.info(getName(), "Map folder: " + mapFolder.getAbsolutePath());
+				Log.info(getName(), "World folder: " + worldFolder.getAbsolutePath());
+			} catch (JSONException | IOException e) {
+				e.printStackTrace();
+				Log.error(getName(), "Failed to read map overrides from file " + mapOverrides.getAbsolutePath());
+			}
+		}
+
+		try {
+			FileUtils.forceMkdir(getDataFolder());
+			FileUtils.forceMkdir(mapFolder);
+			FileUtils.forceMkdir(worldFolder);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Log.fatal(getName(), "Failed to setup data directory");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+
+		// Enable required modules
+		ModuleManager.enable(GameManager.class);
+		ModuleManager.enable(GameLobby.class);
+
+		// Init game and maps
+		this.game = new TNTTag();
+
+		GameManager.getInstance().loadGame(game);
+
+		GUIMapVote mapSelector = new GUIMapVote();
+
+		GameManager.getInstance().setMapSelector(mapSelector);
+
+		// Register events
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		Bukkit.getServer().getPluginManager().registerEvents(mapSelector, this);
+
+		// Read maps
+		Log.info(getName(), "Loading maps from " + mapFolder.getPath());
+		GameManager.getInstance().readMapsFromFolder(mapFolder, worldFolder);
+	}
+
+	@Override
+	public void onDisable() {
+		Bukkit.getScheduler().cancelTasks(this);
+		HandlerList.unregisterAll((Plugin) this);
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onVersionIndependantPlayerAchievementAwarded(VersionIndependantPlayerAchievementAwardedEvent e) {
+		e.setCancelled(true);
+	}
+}
---
+++ src/net/novauniverse/games/tnttag/game/TNTTag.java
@@ -0,0 +1,270 @@
+package net.novauniverse.games.tnttag.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import net.novauniverse.games.tnttag.NovaTNTTag;
+import net.zeeraa.novacore.commons.log.Log;
+import net.zeeraa.novacore.spigot.NovaCore;
+import net.zeeraa.novacore.spigot.abstraction.VersionIndependantUtils;
+import net.zeeraa.novacore.spigot.abstraction.enums.VersionIndependantSound;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.GameEndReason;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.MapGame;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.elimination.PlayerQuitEliminationAction;
+import net.zeeraa.novacore.spigot.utils.PlayerUtils;
+import net.zeeraa.novacore.spigot.utils.RandomFireworkEffect;
+
+public class TNTTag extends MapGame implements Listener {
+	private boolean started;
+	private boolean ended;
+
+	private List<UUID> taggedPlayers;
+	private Map<Player, TNTPrimed> playerTNT;
+
+	public TNTTag() {
+		super(NovaTNTTag.getInstance());
+
+		this.started = false;
+		this.ended = false;
+		this.taggedPlayers = new ArrayList<UUID>();
+		this.playerTNT = new HashMap<Player, TNTPrimed>();
+	}
+
+	@Override
+	public String getName() {
+		return "tnttag";
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "TNT Tag";
+	}
+
+	@Override
+	public PlayerQuitEliminationAction getPlayerQuitEliminationAction() {
+		return NovaTNTTag.getInstance().isAllowReconnect() ? PlayerQuitEliminationAction.DELAYED : PlayerQuitEliminationAction.INSTANT;
+	}
+
+	@Override
+	public boolean eliminatePlayerOnDeath(Player player) {
+		return true;
+	}
+
+	@Override
+	public boolean isPVPEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean autoEndGame() {
+		return true;
+	}
+
+	@Override
+	public boolean hasStarted() {
+		return started;
+	}
+
+	@Override
+	public boolean hasEnded() {
+		return ended;
+	}
+
+	@Override
+	public boolean isFriendlyFireAllowed() {
+		return false;
+	}
+
+	@Override
+	public boolean canAttack(LivingEntity attacker, LivingEntity target) {
+		return true;
+	}
+
+	public void tpToSpectator(Player player) {
+		NovaCore.getInstance().getVersionIndependentUtils().resetEntityMaxHealth(player);
+		player.setHealth(20);
+		player.setGameMode(GameMode.SPECTATOR);
+		if (hasActiveMap()) {
+			player.teleport(getActiveMap().getSpectatorLocation());
+		}
+	}
+	
+	public void spawnPlayerTNT(Player player) {
+		if(playerTNT.containsKey(player)) {
+			return;
+		}
+		TNTPrimed tnt = player.getWorld().spawn(player.getLocation(), TNTPrimed.class);
+		tnt.setFireTicks(Integer.MAX_VALUE);
+		playerTNT.put(player, tnt);
+	}
+
+	/**
+	 * Teleport a player to a provided start location
+	 * 
+	 * @param player   {@link Player} to teleport
+	 * @param location {@link Location} to teleport the player to
+	 */
+	protected void tpToArena(Player player, Location location) {
+		player.teleport(location.getWorld().getSpawnLocation());
+		PlayerUtils.clearPlayerInventory(player);
+		PlayerUtils.clearPotionEffects(player);
+		PlayerUtils.resetPlayerXP(player);
+		player.setHealth(player.getMaxHealth());
+		player.setSaturation(20);
+		player.setFoodLevel(20);
+		player.setGameMode(GameMode.SURVIVAL);
+		player.teleport(location);
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				player.teleport(location);
+			}
+		}.runTaskLater(NovaTNTTag.getInstance(), 10L);
+	}
+
+	@Override
+	public void onStart() {
+		if (started) {
+			return;
+		}
+
+		world.setDifficulty(Difficulty.PEACEFUL);
+
+		List<Player> toTeleport = new ArrayList<Player>();
+
+		Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+			if (players.contains(player.getUniqueId())) {
+				toTeleport.add(player);
+			} else {
+				tpToSpectator(player);
+			}
+		});
+
+		Collections.shuffle(toTeleport);
+
+		List<Location> toUse = new ArrayList<Location>();
+		while (toTeleport.size() > 0) {
+			if (toUse.size() == 0) {
+				for (Location location : getActiveMap().getStarterLocations()) {
+					toUse.add(location);
+				}
+
+				Collections.shuffle(toUse);
+			}
+
+			if (toUse.size() == 0) {
+				// Could not load spawn locations. break out to prevent server from crashing
+				Log.fatal("TNTRun", "The map " + this.getActiveMap().getMapData().getMapName() + " has no spawn locations. Ending game to prevent crash");
+				Bukkit.getServer().broadcastMessage(ChatColor.RED + "TNTRun has run into an uncorrectable error and has to be ended");
+				this.endGame(GameEndReason.ERROR);
+				return;
+			}
+
+			tpToArena(toTeleport.remove(0), toUse.remove(0));
+		}
+
+		// Disable drops
+		this.getActiveMap().getWorld().setGameRuleValue("doTileDrops", "false");
+
+		started = true;
+		sendBeginEvent();
+	}
+
+	@Override
+	public void onEnd(GameEndReason reason) {
+		if (ended) {
+			return;
+		}
+
+		getActiveMap().getStarterLocations().forEach(location -> {
+			Firework fw = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
+			FireworkMeta fwm = fw.getFireworkMeta();
+
+			fwm.setPower(2);
+			fwm.addEffect(RandomFireworkEffect.randomFireworkEffect());
+
+			fw.setFireworkMeta(fwm);
+		});
+
+		Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+			player.setHealth(player.getMaxHealth());
+			player.setFoodLevel(20);
+			PlayerUtils.clearPlayerInventory(player);
+			PlayerUtils.resetPlayerXP(player);
+			player.setGameMode(GameMode.SPECTATOR);
+			VersionIndependantUtils.get().playSound(player, player.getLocation(), VersionIndependantSound.WITHER_DEATH, 1F, 1F);
+		});
+
+		ended = true;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerQuit(PlayerQuitEvent e) {
+		Player player = e.getPlayer();
+		if(taggedPlayers.contains(player.getUniqueId())) {
+			spawnPlayerTNT(player);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		if(playerTNT.containsKey(player)) {
+			playerTNT.get(player).remove();
+			playerTNT.remove(player);
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onPlayerDropItem(PlayerDropItemEvent e) {
+		if (hasStarted()) {
+			if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+				e.setCancelled(true);
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onBlockPlace(BlockPlaceEvent e) {
+		if (hasStarted()) {
+			if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+				e.setCancelled(true);
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent e) {
+		if (hasStarted()) {
+			if (e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+				e.setCancelled(true);
+			}
+		}
+	}
+}
